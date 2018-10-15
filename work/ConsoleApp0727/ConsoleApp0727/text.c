61a810466e20da8477d538e73f4bd5607f8fda07
Song Soo jin <thdtnwls93@naver.com>
2018-10-15 16:13:25 +0900

    #1

---
+++ work/ConsoleApp0727/ConsoleApp0727/text.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(void) {
+
+	printf("Hello c world!!");
+
+	system("pause");
+	return 0;
+
+}
---
+++ work/ConsoleApp0727_1/ConsoleApp0727_1/text0727_1.c
@@ -0,0 +1,37 @@
+
+#include<stdio.h>
+
+int main(void) {
+
+printf("c:\\work \n");
+printf("Hello c world \n");
+printf("sw \" 응용\" 2기 \n");
+system("pause");
+printf("안녕하세요 저는 박성효입니다. \n");
+printf("연락처 010 7919 3575 \n");
+printf("메일주소 parksh-7@daum.net \n");
+system("pause");
+
+return 0;
+}
+
+
+/*
+#include <stdio.h>
+
+int main(void) {
+int answer = 0;
+answer = sum(3, 4);
+printf("%d \n", answer);
+
+system("pause");
+
+return 0;
+}
+
+int sum(int x, int y) {
+int result = 0;
+result = x + y;
+return result;
+}
+*/
